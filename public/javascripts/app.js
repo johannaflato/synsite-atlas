2352bc34b9c606194e77d61d81465809f5804ab1
Seth Thompson <s3th.thompson@gmail.com>
2021-01-25 16:49:14 -0500

    move app.js to footer, wrap script in DOMContentLoaded event

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,19 +1,22 @@
-imagesLoaded('.grid', () => {
-
-  const images = Array.from(document.querySelectorAll('img'));
-  for (let image of images) {
-    // triggers fade-in
-    image.classList.add('img--loaded')
-  }
-
-  const packery = new Packery('.grid', {
-    itemSelector: '.grid-block',
-    gutter: 5,
-    percentPosition: true,
-  });
-
-  for (let item of packery.items) {
-    const drag = new Draggabilly(item.element);
-    packery.bindDraggabillyEvents(drag);
-  }
+document.addEventListener("DOMContentLoaded", () => {
+  imagesLoaded('.grid', () => {
+    const images = Array.from(document.querySelectorAll('img'));
+    for (let image of images) {
+      // triggers fade-in
+      image.classList.add('img--loaded')
+    }
+  
+    // sets up grid layout
+    const packery = new Packery('.grid', {
+      itemSelector: '.grid-block',
+      gutter: 5,
+      percentPosition: true,
+    });
+  
+    // adds draggable behavior
+    for (let item of packery.items) {
+      const drag = new Draggabilly(item.element);
+      packery.bindDraggabillyEvents(drag);
+    }
+  });  
 });
