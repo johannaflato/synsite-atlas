00e9f9d9c2129d5b77a90ba0523e99b1c73e3b9f
Johanna Flato <jcflato@gmail.com>
2021-02-28 05:21:31 -0500

    attempt at modal/lightbox

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -41,3 +41,34 @@ function toggleChannels(){
     c.style.display = "none";
   }
 }
+
+//modal functions
+function openModal() {
+  document.getElementById("blockModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("blockModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide-block");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
