ccbb2e1f7f873c8f4e1616e1facf594bddf8ebee
Johanna Flato <jcflato@gmail.com>
2021-03-02 16:56:41 -0500

    created (currently inactive) click-to-expand functionality 4 grid-blocks

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,37 +1,35 @@
+// init Packery
 var grid = document.querySelector('.grid');
+var pckry;
 
-document.addEventListener("DOMContentLoaded", () => {
-  imagesLoaded(grid, () => {
-    const images = Array.from(document.querySelectorAll('img'));
-    for (let image of images) {
-      // triggers fade-in
-      image.classList.add('img--loaded')
-    }
+imagesLoaded( grid, function() {
+  const images = Array.from(document.querySelectorAll('img'));
+  for (let image of images) {
+    image.classList.add('img--loaded')
+  }
+  // init Packery after all images have loaded
+  pckry = new Packery( grid, {
+    itemSelector: '.grid-block',
+    gutter: 5,
+    percentPosition: false,
+    stamp: '.stamp',
+    stagger: 20,
+  });
 
-    // sets up grid layout
-    const packery = new Packery(grid, {
-      itemSelector: '.grid-block',
-      gutter: 5,
-      percentPosition: false,
-      stamp: '.stamp',
-      // stagger: 30,
-    });
-
-    // grid.addEventListener( 'click', function( event ) {
-    //   // filter for grid-block clicks
-    //   if (!event.target.classList.contains("grid-block") ) {
-    //     return;
-    //   }
-    //   event.target.classList.toggle("grid-block--large");
-    //   packery.layout();
-    // });
   // adds draggable behavior
   for (let item of pckry.items) {
     const drag = new Draggabilly(item.element);
     pckry.bindDraggabillyEvents(drag);
   }
 
-  });
+  // // expand grid-blocks to larger size
+  // grid.addEventListener( 'click', function( event ) {
+  //   if ( !event.target.parentNode.classList.contains('dimensions') ) {
+  //     return;
+  //   }
+  //   event.target.parentNode.classList.replace("dimensions", "dimensions--zoom");
+  //   pckry.layout();
+  // });
 });
 
 // toggle functions
@@ -53,33 +51,38 @@ function toggleChannels(){
   }
 }
 
+//go back 'x'
+function goBack() {
+  window.history.back();
+}
+
 //modal functions
-function openModal() {
-  document.getElementById("blockModal").style.display = "block";
-}
-
-function closeModal() {
-  document.getElementById("blockModal").style.display = "none";
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slide-block");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  slides[slideIndex-1].style.display = "block";
-}
+// function openModal() {
+//   document.getElementById("blockModal").style.display = "block";
+// }
+//
+// function closeModal() {
+//   document.getElementById("blockModal").style.display = "none";
+// }
+//
+// var slideIndex = 1;
+// showSlides(slideIndex);
+//
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+//
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+//
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("slide-block");
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//   }
+//   slides[slideIndex-1].style.display = "block";
+// }
