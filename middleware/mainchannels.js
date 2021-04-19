bb0b501aba53e6d925a895dea2e580e5b02e90bc
Seth Thompson <s3th.thompson@gmail.com>
2021-04-19 15:14:48 -0400

    add main channels

--- middleware/mainchannels.js
+++ middleware/mainchannels.js
@@ -1,11 +1,14 @@
 const Arena = require('are.na');
 
+const { ARENA_CHANNEL_ID } = process.env;
+
 module.exports = async (req, res, next) => {
   const arena = new Arena();
-  const data = arena.channel('syn-site').get(); //removed 'await'
+  const data = await arena.channel(ARENA_CHANNEL_ID).get();
   // filter syn-site channel's blocks by the 'Channel' class
   const mainchannels = data.contents
     .filter(block => block.class == 'Channel');
+
   res.locals.mainchannels = mainchannels;
   next();
 };
