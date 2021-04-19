a79f0da1fa298f17e4a4f54970d8097cdb446d7f
Seth Thompson <s3th.thompson@gmail.com>
2021-04-19 15:51:14 -0400

    add pagination

--- middleware/mainchannels.js
+++ middleware/mainchannels.js
@@ -1,10 +1,11 @@
 const Arena = require('are.na');
+const { paginatedChannelGet } = require('../lib/api');
 
 const { ARENA_CHANNEL_ID } = process.env;
 
 module.exports = async (req, res, next) => {
   const arena = new Arena();
-  const data = await arena.channel(ARENA_CHANNEL_ID).get();
+  const data = await paginatedChannelGet(arena, ARENA_CHANNEL_ID);
   // filter syn-site channel's blocks by the 'Channel' class
   const mainchannels = data.contents
     .filter(block => block.class == 'Channel');
--- routes/channels.js
+++ routes/channels.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const Arena = require('are.na');
+const { paginatedChannelGet } = require('../lib/api');
 
 router.get('/:id', async (req, res) => {
   if (!res.locals.definition) next(); // 404
   const arena = new Arena();
-  const data = await arena.channel(req.params.id).get()
+  const data = await paginatedChannelGet(arena, req.params.id);
   res.render('channel', data)
 });
 
