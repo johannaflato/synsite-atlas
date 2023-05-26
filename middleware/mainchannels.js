27597465969624e67f29c5835f48e99a48e8ef9b
johannaflato <jcflato@gmail.com>
2023-05-25 23:41:55 -0400

    added syn-site as default for the .env

--- middleware/mainchannels.js
+++ middleware/mainchannels.js
@@ -1,11 +1,11 @@
 const Arena = require('are.na');
 const { paginatedChannelGet } = require('../lib/api');
 
-const { ARENA_CHANNEL_ID } = process.env;
+const ARENA_CHANNEL_ID = process.env.ARENA_CHANNEL_ID || "syn-site";
 
 module.exports = async (req, res, next) => {
   const arena = new Arena();
-  const data = await paginatedChannelGet(arena, 'syn-site');
+  const data = await paginatedChannelGet(arena, ARENA_CHANNEL_ID);
   // filter syn-site channel's blocks by the 'Channel' class
   const mainchannels = data.contents
     .filter(block => block.class == 'Channel');
