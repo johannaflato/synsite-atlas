f11513c849d528695986607c918804abc64d3a03
johannaflato <jcflato@gmail.com>
2024-04-29 01:22:06 -0400

    remove jsonwebtoken
    and the express duplication

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
-const jwt = require('jsonwebtoken');
 
-const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
